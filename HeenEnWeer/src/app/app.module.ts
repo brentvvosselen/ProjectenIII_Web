3235f46904753744be45a7b1407f59a3c53fd60d
Dylan <NeleVM@MacBook-Pro-van-Nele.local>
2017-10-04 17:29:09 +0200

    toevoeging van auth service en login component

--- HeenEnWeer/src/app/app.module.ts
+++ HeenEnWeer/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import {AuthService} from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: "login", component: LoginComponent}
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent,NavbarComponent]
 })
 export class AppModule { }
---
+++ HeenEnWeer/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  test: string = 'just a test';
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    let sampleUser: any = {
+      email: 'michael@realpython.com' as string,
+      password: 'michael' as string
+    };
+    this.auth.register(sampleUser)
+    .then((user) => {
+      console.log(user.json());
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    this.auth.login(sampleUser).then((user) => {
+      console.log(user.json());
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+  
+}
