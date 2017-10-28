a5b9f2d59f2339970491e09a87de49d1dcdfe658
Dylan <NeleVM@MacBook-Pro-van-Nele-2.local>
2017-10-28 10:26:53 +0200

    check of user setup doorlopen heeft extra wijzigingen in homepagina en navigatie

--- HeenEnWeer/src/app/home/home.component.ts
+++ HeenEnWeer/src/app/home/home.component.ts
@@ -3,37 +3,32 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../app/models/user';
 import { UserService } from '../services/user-service.service';
 import { ViewEncapsulation } from '@angular/core';
+import { ParentService } from '../services/parent.service';
+import { AuthenticationService } from '../services/authentication-service.service';
 
 @Component({
-   moduleId: module.id,
-   templateUrl: 'home.component.html',
-   styleUrls: ['./home.component.css'],
-   encapsulation: ViewEncapsulation.None
+    moduleId: module.id,
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class HomeComponent implements OnInit {
-   currentUser: User;
-   users: User[] = [];
+    currentUser: User;
+    user: User;
 
-   constructor(private userService: UserService) {
-       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-   }
+    constructor(private userService: UserService, private authenticationService: AuthenticationService, private parentService: ParentService) {
+        this.user = this.authenticationService.getUser();
+    }
 
-   ngOnInit() {
-       this.loadAllUsers();
-   }
+    ngOnInit() {
+        this.getParentFromUserEmail(this.user.email);
+    }
 
-   /*
-   deleteUser(id: number) {
-       this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
-   }
-   */
-
-   private loadAllUsers() {
-       this.userService.getAll().subscribe(users => { this.users = users; });
-   }
-
-   requestSecretRoute(){
-       this.userService.requestSecretRoute().subscribe();
-   }
+    private getParentFromUserEmail(email: string) {
+        this.parentService.getByEmail(email).map(
+            (response) => this.currentUser = response).subscribe(data => {
+                console.log(data);
+            });
+    }
 }
