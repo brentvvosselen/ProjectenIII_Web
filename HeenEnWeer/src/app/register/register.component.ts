82911b391b082f1a71b0955a5b969a893a20cace
DESKTOP-V5KVRI6\Dylan <dylan_verstraete1@hotmail.com>
2017-12-05 14:56:53 +0100

    refactoring register form errors op submit

--- HeenEnWeer/src/app/register/register.component.ts
+++ HeenEnWeer/src/app/register/register.component.ts
@@ -7,45 +7,51 @@ import { ViewEncapsulation } from '@angular/core';
 import { UserService } from '../../app/services/user-service.service';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'register.component.html',
-    styleUrls: ['./register.component.css'],
-    encapsulation: ViewEncapsulation.None
+  moduleId: module.id,
+  templateUrl: 'register.component.html',
+  styleUrls: ['./register.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class RegisterComponent implements OnInit {
-    model: any = {};
-    loading = false;
+  model: any = {};
+  loading = false;
 
-    registerForm: FormGroup;
+  registerForm: FormGroup;
 
-    constructor(private router: Router, private userService: UserService, private fb: FormBuilder) { }
+  constructor(private router: Router, private userService: UserService, private fb: FormBuilder) { }
 
-    ngOnInit() {
-      this.registerForm = this.fb.group({
-        email: ['', [Validators.required, Validators.minLength(3), Validators.pattern('[a-z0-9._%+-]+@[a-z0-9]+\\.[a-z]{2,3}')]],
-        lastname: ['', [Validators.required, Validators.minLength(3)]],
-        firstname: ['', [Validators.required, Validators.minLength(3)]],
-        password: ['', [Validators.required, Validators.minLength(3)]],
-        passwordConfirm: ['', [Validators.required, Validators.minLength(3), matchOtherValidator('password')]],
-      });
-    }
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(3), Validators.pattern('[a-z0-9._%+-]+@[a-z0-9]+\\.[a-z]{2,3}')]],
+      lastname: ['', [Validators.required, Validators.minLength(3)]],
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      passwordConfirm: ['', [Validators.required, Validators.minLength(3), matchOtherValidator('password')]],
+    });
+  }
 
-    register() {
+
+  register() {
+    if (this.registerForm.valid) {
       console.log("TEST");
-        this.loading = true;
+      this.loading = true;
 
-        this.model = {
-          password: this.registerForm.get('password').value,
-          firstName: this.registerForm.get('firstname').value,
-          lastName: this.registerForm.get('lastname').value,
-          email: this.registerForm.get('email').value
-        };
+      this.model = {
+        password: this.registerForm.get('password').value,
+        firstName: this.registerForm.get('firstname').value,
+        lastName: this.registerForm.get('lastname').value,
+        email: this.registerForm.get('email').value
+      };
 
-        this.userService.create(this.model).subscribe(data => {
-          this.router.navigate(['/login']);
-        }, error => {
-          this.loading = false;
-        });
+      this.userService.create(this.model).subscribe(data => {
+        this.router.navigate(['/login']);
+      }, error => {
+        this.loading = false;
+      });
+    } else {
+      console.log("not valid");
     }
+
+  }
 }
