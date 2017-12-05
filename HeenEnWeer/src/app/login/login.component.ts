710685c3d7a57f657d745a1ca6d7355c668e346a
DESKTOP-V5KVRI6\Dylan <dylan_verstraete1@hotmail.com>
2017-12-05 15:18:41 +0100

    refactoring login scherm

--- HeenEnWeer/src/app/login/login.component.ts
+++ HeenEnWeer/src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
-import {AuthenticationService} from "../services/authentication-service.service";
+import { HttpHeaders } from '@angular/common/http';
+import { AuthenticationService } from "../services/authentication-service.service";
 import { Router, ActivatedRoute } from '@angular/router';
 import { ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -16,31 +17,44 @@ export class LoginComponent implements OnInit {
   model: any = {};
   loading = false;
   returnUrl: string;
+  loginForm: FormGroup;
 
-  constructor(private http:HttpClient,
-      private authenticationService: AuthenticationService,
-      private router: Router,private route: ActivatedRoute,) {}
+  constructor(private http: HttpClient,
+    private authenticationService: AuthenticationService,
+    private router: Router, private route: ActivatedRoute,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     // reset login status
     this.authenticationService.logout();
 
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  private onLoginSuccesFull(){
+  private onLoginSuccesFull() {
 
   }
 
   login() {
-    this.loading = true;
-    
-    this.authenticationService.login(this.model.email, this.model.password).subscribe(data => {
-      this.router.navigate([this.returnUrl]);
-    }, error => {
-      this.error = "Combinatie email - paswoord is niet correct";
-      this.loading = false;
-    });
+    if (this.loginForm.valid) {
+      this.model = {
+        password: this.loginForm.get('password').value,
+        email: this.loginForm.get('email').value
+      };
+      this.authenticationService.login(this.model.email, this.model.password).subscribe(data => {
+        this.router.navigate([this.returnUrl]);
+      }, error => {
+        this.error = "Combinatie email - paswoord is niet correct";
+        this.loading = false;
+      });
+    } else {
+      console.log("not valid");
+    }
   }
 }
+
