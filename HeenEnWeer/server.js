9d3b4201153a9d669b937aaae090b897d6c956a1
Joshi <joshi@MacBook-Pro-van-Joshi.local>
2017-12-15 14:36:21 +0100

    disable etag server voor caching android

--- HeenEnWeer/server.js
+++ HeenEnWeer/server.js
@@ -49,6 +49,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
+app.disable('etag');
+
 // connect to database
 mongoose.connect(config.database);
 
