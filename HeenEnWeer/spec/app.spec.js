0d30aa386b6ba0aaf74a91426c07476f93df888b
DESKTOP-V5KVRI6\Dylan <dylan_verstraete1@hotmail.com>
2017-12-17 10:58:11 +0100

    toevoeging post api child test en get parent by email

--- HeenEnWeer/spec/app.spec.js
+++ HeenEnWeer/spec/app.spec.js
@@ -1,5 +1,6 @@
 let Request = require('request');
-
+let token = {};
+let id = {};
 
     //right login gets token and status 200
     describe("POST /api/login",() => {
@@ -48,5 +49,88 @@ let Request = require('request');
 
         it("token empty",() => {
             expect(data.token).toBeUndefined();
+        });
+    });
+
+    describe("GET /api/parents/:email",() => {
+        let data = {};
+        beforeAll((done) => {
+            Request({
+                method: 'GET',
+                uri: 'http://localhost:5000/api/parents/jess@aarschot.be',
+                json: true,
+            },(error, response, body) => {
+                data.status = response.statusCode;
+                data._id = response.body._id;
+                id = data._id;
+                done();
+            }).auth(null,null,true,token);;
+        });
+
+        it("status 200", () => {
+            expect(data.status).toBe(200);
+        });
+
+        it("parent not empty",() => {
+            expect(data._id).toBeDefined();
+        });
+    });
+
+    describe("GET /api/parents/:email",() => {
+        let data = {};
+        beforeAll((done) => {
+            Request({
+                method: 'GET',
+                uri: 'http://localhost:5000/api/parents/jessica@aarschot.be',
+                json: true,
+            },(error, response, body) => {
+                data.status = response.statusCode;
+                data.body = response.body;
+                done();
+            }).auth(null,null,true,token);;
+        });
+
+        it("status 200", () => {
+            expect(data.status).toBe(200);
+        });
+
+        it("parent is empty",() => {
+            expect(data.body).toBeNull();
+        });
+    });
+
+    describe("POST /api/child/:id",() => {
+        let data = {};
+        beforeAll((done) => {
+            Request({
+                method: 'POST',
+                uri: 'http://localhost:5000/api/child/' + id,
+                json: true,
+                body: {
+                    firstname: "ella",
+                    lastname: "aarschot",
+                    gender: "F",
+                    birthdate: new Date()
+                }
+            },(error, response, body) => {
+                data.status = response.statusCode;
+                data.body = response.body;
+                done();
+            }).auth(null,null,true,token);;
+        });
+
+        it("status 200", () => {
+            expect(data.status).toBe(200);
+        });
+
+        it("child is not empty",() => {
+            expect(data.body._id).toBeDefined();
+        });
+
+        it("check recipe body", () => {
+            expect(data.body.firstname).toBe("ella");
+            expect(data.body.lastname).toBe("aarschot");
+            expect(data.body.gender).toBe("F");
+            _id = data.body._id;
         });
     });
